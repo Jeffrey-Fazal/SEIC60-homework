2a0863da00ee7a030a29e60c95989bc56cd3b1a0
Jeffrey Fazal <jeffreyfazal@gmail.com>
2022-12-06 22:55:32 +1100

    Solutions uploaded

---
+++ 06-Jeffrey-Fazal/wk01/2-tue/js-primitives-conditionals.md
@@ -0,0 +1,61 @@
+# JS Primitives & Conditionals
+
+### Getting Started
+
+1. Create two files `index.html` and `script.js`
+1. Include `script.js` in `index.html` using a `<script>` tag
+1. Open `index.html` in your web browser and open the console
+1. After answering each section below, `git commit` your work
+1. When you're finished push to your fork and file a Pull Request to the main homework repo
+
+---
+
+**Pro tip**: Comment out your solution to each section before moving on, so your console only has messages from the section you're currently working on.
+
+### Section 1
+
+Create a variable called `age` and assign it to your age (or any arbitrary number). Write a conditional that matches the following conditions:
+
+  - if `age` is less than or equal 19, output `"You're still a teenager!"`
+
+---
+
+### Section 2
+Create a variable called `a` and assign it a number. Write a conditional that matches the following conditions:
+
+  - if a % 2 is equal to 0, output `"After running our calculations, the result is even"`
+  - otherwise, output `"After running our calculations, the result is odd"`
+
+---
+
+### Section 3
+
+Create a variable called `b` and assign it a random number between 0 and 100. Write a conditional that matches the following conditions:
+
+  - if `b` is less than or equal to 25, output `"bottom quartile"`
+  - if `b` is between 25 and/or equal to 50, output `"lower quartile"`
+  - if `b` is between 50 and/or equal to 75, output `"upper quartile"`
+  - if `b` is between 75 and/or equal to 100, output `"top quartile"`
+
+---
+
+### Section 4
+
+_On the menu tonight: Steak, fruit salad, tofurkey, and pork chops._
+
+Ask the user (use the [`prompt()`](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt) function) for their menu choice and assign it to a variable.  Write a conditional that matches the following conditions:
+
+  - If a vegan can eat it, output `"This cuisine is vegan friendly."`
+  - Otherwise, output `"Vegans beware!"`
+
+For the sake of this exercise, Tofurkey is definitely vegan friendly.
+
+**Hint**: Two identical strings are considered to be equal to each other.
+
+--- 
+
+### Section 5
+
+- Implement the flowchart below in code.
+
+![Flowchart](https://i.imgur.com/m9UJDTy.png)
---
+++ 06-Jeffrey-Fazal/wk01/2-tue/script.js
@@ -0,0 +1,87 @@
+// Section 1
+let age = 1;
+function lifeCycle(age_arg) {
+    if (age_arg <= 19) {
+        console.log("You're still a teenager!")
+        return "You're still a teenager!";
+    } else {
+        console.log("Congrats you are over 19")
+        return "You're older than 19 (an adult is most places)";
+    }
+}
+
+// Try DOM attempt later
+// function findAge() { 
+//     x = document.querySelector('age-field').value;
+//     document.getElementById("yourAge").innerHTML = x;
+//     lifeCycle(x);
+//     return x;
+// }
+
+lifeCycle(age)
+
+// Section 2 - % 0 is even and 1 is odd (old divided by 2 trick to determine even/odd)
+const a = 20 % 2
+if (a === 0) {
+    console.log("After running our calculations, the result is even")
+} else {
+    console.log("After running our calculations, the result is odd")
+}
+
+// Section 3 
+const b = 4
+if (b <= 25) {
+    console.log('bottom quartile')
+} else if (b >= 25 && b <= 50) {
+    console.log('lower quartile')
+} else if (b >= 50 && b <= 75) {
+    console.log('upper quartile')
+} else if (b >= 75 && b <= 100) {
+    console.log('top quartile')
+} // doesn't seem to need: else {}
+
+// Section 4
+
+// Find out how to use array
+// foodOptions = {
+//     veganFoods: 'Fruit salad, Tofurkey',
+//     notVegan: 'Steak, Pork Chops',
+// }
+
+veganFoods = ['Fruit salad', 'Tofurkey']
+nonVeganFoods = ['Steak', 'Pork Chops']
+
+let foodChoice = prompt(`What would you choose from ${veganFoods} & ${nonVeganFoods}? (type options exactly as shown)`);
+
+if (foodChoice === 'Fruit salad' || foodChoice === 'Tofurkey') { // maybe a for loop to use the array?
+    console.log("This cuisine is vegan friendly")
+} else if (foodChoice === 'Steak' || foodChoice === 'Pork Chops') {
+    console.log("Vegans beware!")
+} else {
+    console.log(`You must choose between: ${veganFoods} and ${nonVeganFoods}`)
+}
+
+console.log(foodChoice)
+
+// Section 5
+
+
+const movement = true
+let shouldMove = true
+console.log("Does it move?")
+if (movement === true) {
+    //console.log('Should it move?')
+}  if (shouldMove === true && movement === true) {
+    console.log('No Problem')
+}  if (shouldMove === false && movement === true) {
+    console.log("Duct tape")
+} 
+
+
+if (movement === false) {
+    //console.log("Should it move?")
+}  if (shouldMove === true && movement === false) {
+    console.log("WD-40")
+}  if (shouldMove === false && movement === false) {
+    console.log("No problem")
+}
