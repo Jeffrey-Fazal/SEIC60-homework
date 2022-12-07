e12a934047dde017d66d8c3bb025ed5382e89250
Jeffrey Fazal <jeffreyfazal@gmail.com>
2022-12-08 07:53:25 +1100

    Up 2 leap years

--- 06-Jeffrey-Fazal/wk01/3-wed/functions.js
+++ 06-Jeffrey-Fazal/wk01/3-wed/functions.js
@@ -3,16 +3,18 @@
 const calculate = function (num1, num2, operation) {
     if (operation === 'add') {
         console.log(num1 + num2)
-    } else if (opperation === 'sub') {
+    } else if (operation === 'sub') {
         console.log(num1 - num2)
-    } else if (opperation === 'multi') {
+    } else if (operation === 'multi') {
         console.log(num1 * num2)
-    } else if (opperation === 'div') {
+    } else if (operation === 'div') {
         console.log(num1 / num2)
-    } else if (opperation === 'exp') {
+    } else if (operation === 'exp') {
         console.log(num1 ^ num2)
     }
 }
+
+console.log(calculate(4,10,'sub'))
 
 // helloIn
 const helloEng = 'Hello'
@@ -46,3 +48,18 @@ helloIn('French')     // -> "Bonjour"
 helloIn('Dog')        // -> "Woof"
 
 // leapYears 
+// Provided by fung: 
+// Definition of leap years = Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are.It is fun to know that 1900 is not a leap year!
+
+const leapYears = function (year) {
+    //if divisible by 4 || not divisible by 100
+    if (year % 2 === 0) {
+        console.log('posible a leap year in ' + year)
+        if (year % 100 !== 0) {
+            console.log('not a leap year ' + year)
+        } else if (year % 100 === 0) {
+            console.log('still a leap year ' + year)
+        }
+    }
+}
+leapYears(1944)
